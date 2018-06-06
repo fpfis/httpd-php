82d8dc2738cde72b40db4cf807c071a6d7a7393f
tiago-fm <tiago.mauricio@ext.ec.europa.eu>
2018-06-06 16:49:57 +0200

    Added modules

--- install-ext-modules.sh
+++ install-ext-modules.sh
@@ -2,4 +2,61 @@
 
 set -xue
 
-apk add --no-cache --virtual .persistent-deps ca-certificates curl tar xz libressl 
+modules="bz2 calendar exif gd pdo_mysql opcache zip xsl intl mcrypt ldap "
+
+
+#Dumb list of dev dependencies...
+makedepends="
+	autoconf
+	apache2-dev
+	aspell-dev
+	bison
+	bzip2-dev
+	curl-dev
+	db-dev
+	enchant-dev
+	freetds-dev
+	freetype-dev
+	gdbm-dev
+	gettext-dev
+	gmp-dev
+	icu-dev
+	imap-dev
+	krb5-dev
+	libedit-dev
+	libical-dev
+	libjpeg-turbo-dev
+	libmcrypt-dev
+	libpng-dev
+	libressl-dev
+	libwebp-dev
+	libxml2-dev
+	libxpm-dev
+	libxslt-dev
+	libzip-dev
+	net-snmp-dev
+	openldap-dev
+	pcre-dev
+	postgresql-dev
+	re2c
+	recode-dev
+	sqlite-dev
+	tidyhtml-dev
+	unixodbc-dev
+	zlib-dev
+	"
+
+apk add --no-cache --virtual .build-deps $makedepends $PHPIZE_DEPS
+
+docker-php-source extract
+
+pecl install igbinary
+
+docker-php-ext-enable igbinary
+
+
+
+docker-php-ext-install $modules
+
+
+
