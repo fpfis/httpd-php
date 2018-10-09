a37f49ad76e75428d58fd0335354feeed0ac1f04
Gregory Boddin <gregory@siwhine.net>
2018-10-09 14:12:49 +0200

    Added script testing ubuntu build

--- scripts/install-base.sh
+++ scripts/install-base.sh
@@ -10,7 +10,7 @@ for module in ${php_modules}; do
   modules="php${php_version}-${module} ${modules}"
 done
 
-apt-get install -y php${php_version}-fpm ${modules}
+apt-get install -y apache2 php${php_version}-fpm ${modules}
 
 apt-get autoremove software-properties-common -y --purge
 apt-get clean
