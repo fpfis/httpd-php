2abb461015e21814ca74219fc6b0ea4cbcd57749
SCHWARTZ Anthony <anthony.schwartz@ext.ec.europa.eu>
2018-12-28 15:33:08 +0100

    wt - ubuntu - php - cleanup run script

--- run.sh
+++ run.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
 
-# Get uid for the current docroot
-[ -z "${DOCUMENT_ROOT}" ] && export DOCUMENT_ROOT=/var/www/html
-
-# Check if docroot or parent exists :
-[ -d "${DOCUMENT_ROOT}" ] && export REF_DIR=${DOCUMENT_ROOT}
-# Else use its parent
-[ -z "${REF_DIR}" ] && export REF_DIR=$(dirname ${DOCUMENT_ROOT})
-
 # Get our command to run
 export CMD="${@}"
-
-# If APACHE_EXTRA_CONF isn't being set outside, set it to an empty value.
-if [ -z ${APACHE_EXTRA_CONF} ]; then export APACHE_EXTRA_CONF=""; fi
 
 if [ -z "${CMD}" ]; then
   # If no run command provided, run supervisor as root a:
