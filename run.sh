c1b982c24900bdaf5df16557521ddbd6a2639929
SCHWARTZ Anthony <anthony.schwartz@ext.ec.europa.eu>
2018-12-31 08:59:54 +0100

    wt - ubuntu - php - Cleanup DockerFile (typo)

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export CMD="${@}"
 
 if [ -z "${CMD}" ]; then
   # If no run command provided, run supervisor as root a:
-  /usr/bin/supervisord -cn /etc/supervisor/supervisord.conf
+  /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
 else
   # Run the command as user web
   eval "${CMD}"
