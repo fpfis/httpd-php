7d09c60ca5e840cc02c3b106cab105979ee3fc16
Voidtek <vdacosta@voidtek.com>
2019-02-28 12:50:41 +0100

    Update installation of blackfire on dev.

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -7,8 +7,7 @@ apt-get update
 apt-get install -y gnupg wget ${dev_packages}
 
 # Install blackfire :
-
-wget -q -O - https://packagecloud.io/gpg.key | apt-key add -
+wget -q -O - https://packages.blackfire.io/gpg.key | apt-key add -
 echo "deb http://packages.blackfire.io/debian any main" | tee /etc/apt/sources.list.d/blackfire.list
 apt-get update
 apt-get install -y blackfire-php
