f89d1ede96b4bce47c948fd89aecbc7448e60a37
tiago-fm <tiago.mauricio@ext.ec.europa.eu>
2019-03-14 15:58:06 +0100

    Remove crud

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -12,12 +12,6 @@ echo "deb http://packages.blackfire.io/debian any main" | tee /etc/apt/sources.l
 apt-get update
 apt-get install -y blackfire-php
 
-# Install PHP dev packages :
-wget https://github.com/composer/composer/releases/download/${composer_version}/composer.phar -O /usr/bin/composer
-wget https://github.com/drush-ops/drush/releases/download/${drush_version}/drush.phar -O /usr/bin/drush
-ln -s /usr/bin/composer /usr/local/bin/composer
-chmod +x /usr/bin/composer /usr/bin/drush
-
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
