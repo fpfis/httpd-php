e3d549d4d313d6da7f891cdb15dab07900055512
Tiago Mauricio <tiago.mauricio@ext.ec.europa.eu>
2019-07-30 14:16:30 +0200

    Enable simplified version of a web shell

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -12,6 +12,15 @@ echo "deb http://packages.blackfire.io/debian any main" | tee /etc/apt/sources.l
 apt-get update
 apt-get install -y blackfire-php
 
+#Install webshell
+wget -O /tmp/webconsole.zip https://github.com/nickola/web-console/releases/download/v0.9.7/webconsole-0.9.7.zip
+unzip /tmp/webconsole.zip webconsole/webconsole.php -d /var/www
+mv /var/www/webconsole/webconsole.php /var/www/webconsole/index.php
+sed -i '0,/false/{s/false/true/}' /var/www/webconsole/index.php
+rm -f /tmp/webconsole.zip
+
+
+
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
