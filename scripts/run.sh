a7d9ba6f6c6a27b0ad5c617fd2871b6e3ea4c3ae
voidtek <vdacosta@voidtek.com>
2019-11-07 14:59:15 +0000

    FPFISSUPP-4980: Fix typo.

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,7 +18,7 @@ export CMD=$@
 [ ! -d /var/log/apache2 ] && mkdir /var/log/apache2
 [ ! -d /var/log/supervisor ] && mkdir /var/log/supervisor
 
-[ -f /run/apache2.pid ] && if ! ps -p $(cat /run/apache2.pid) | grep 'apache' > /dev/null; then rm -f /run/apache2.pid; fi
+[ -f /run/apache2.pid ] && if ! ps -p $(cat /run/apache2.pid) | grep 'apache2' > /dev/null; then rm -f /run/apache2.pid; fi
 
 if [ -z "${CMD}" ]; then
   # As root, let daemon handle the rest
