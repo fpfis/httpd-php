18ccb89c320083e9c8f3fbbdaaba10aa86809f00
Tiago Mauricio <tiago.mauricio@ext.ec.europa.eu>
2020-01-09 11:35:00 +0100

    Added exec to supervisord to make it pid 1, saving 1 process

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,7 +22,7 @@ export CMD=$@
 
 if [ -z "${CMD}" ]; then
   # As root, let daemon handle the rest
-  supervisord -nc /etc/supervisor/supervisord.conf
+  exec supervisord -nc /etc/supervisor/supervisord.conf
 else
   # TODO : us ref_dir's permissions to use it's UID
   exec ${CMD}
